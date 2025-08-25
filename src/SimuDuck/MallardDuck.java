eebeab5e7e2950ee0f68299cd47e84ae9056c5f5
MithunW <mithunmedia123@gmail.com>
2025-08-25 02:08:01 +0100

    HeadFirstDesignPatterns Learning, Testing and Revising Low Level System Design and Design Patterns
    #Referencing the Head First Design Patterns Second Edition Book
    
    Separate changing behavior from unchanging behavior and composition it with separate classes.
    Prioritize composition over inheritance.
    Extensibility and maintainability is important over code reuse.

---
+++ src/SimuDuck/MallardDuck.java
@@ -0,0 +1,12 @@
+package SimuDuck;
+
+public class MallardDuck extends Duck {
+    public MallardDuck() {
+        quackBehavior = new Quack();
+        flyBehavior = new FlyWithWings();
+    }
+
+    public void display() {
+        System.out.println("I'm a MallardDuck!");
+    }
+}
